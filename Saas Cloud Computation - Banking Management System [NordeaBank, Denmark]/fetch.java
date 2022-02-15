1472d9e3f17bc6514cf452f7ee5560b14423e06e
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-16 00:06:51 +0530

    Update fetch.java

--- Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/fetch.java
+++ Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/fetch.java
@@ -3,7 +3,7 @@ import java.sql.*;
         
 class fetch        
 {    
-public static void main(String args[])        
+public static void main(String args[])         
 {      
 try{ 
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
