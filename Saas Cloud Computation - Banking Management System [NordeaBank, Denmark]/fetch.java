eaf01497b0469f82ccab91743df322b4d522414b
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-16 00:02:32 +0530

    Update fetch.java

--- Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/fetch.java
+++ Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/fetch.java
@@ -4,7 +4,7 @@ import java.sql.*;
 class fetch 
 {
 public static void main(String args[]) 
-{ 
+{  
 try{
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 System.out.println("Enter SID");
