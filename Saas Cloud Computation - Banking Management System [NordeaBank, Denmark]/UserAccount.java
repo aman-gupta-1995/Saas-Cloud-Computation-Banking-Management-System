8aac0b1e0f2b2734b40954c3ae047ad164672b6c
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-08 16:40:50 +0530

    Update UserAccount.java

--- Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/UserAccount.java
+++ Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/UserAccount.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.Date; 
 
 class UserAccount {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
 
     public void createAccount() throws Exception { 
         int accno = 0;
