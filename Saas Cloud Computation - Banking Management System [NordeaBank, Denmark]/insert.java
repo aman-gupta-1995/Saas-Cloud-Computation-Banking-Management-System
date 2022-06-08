12bea7ce3f016de5cc78e89d387efefa87b70cda
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-08 16:43:07 +0530

    Update insert.java

--- Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/insert.java
+++ Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/insert.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 class insert
 {
-public static void main(String args[])
+public static void main(String args[]) 
 {
 try{
 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
