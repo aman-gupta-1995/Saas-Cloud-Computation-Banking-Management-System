f9e2124d9b0c34362ba548e592fedc98608b77ae
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-08 16:42:35 +0530

    Update connect.java

--- Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/connect.java
+++ Saas Cloud Computation - Banking Management System [NordeaBank, Denmark]/connect.java
@@ -7,7 +7,7 @@ public Connection fetchcon()
 { 
 try
 {
-Class.forName("oracle.jdbc.OracleDriver");
+Class.forName("oracle.jdbc.OracleDriver"); 
 con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","123456");
 }
 catch(Exception e)
